0122b7a02033ce86800cfff4a384269bda254f4a
215254589 <47609837+215254589@users.noreply.github.com>
2020-02-19 13:05:42 +0200

    Add files via upload
    modified

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -4,8 +4,8 @@ public class Project {
         public static void main(String[] args) {
             int num1 = 5, num2 = 15, sum;
             sum = num1 + num2;
-
-            System.out.println("Sum of these numbers: " + sum);
+            int sum2= num1/num2;
+            System.out.println("Sum of these numbers: " + sum+" division of these numbers are "+sum2);
         }
     }
 }
